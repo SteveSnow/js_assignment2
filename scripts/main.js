bfa0aa185b44cae0b678717c2d065432f25de231
SteveSnow <Steven.Snow@outlook.com>
2016-02-12 14:34:14 -0500

    updated formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
     $('#startBtn').click(function() {
-        setTimer(60)
+        setTimer(60);
     })
 })
 
 function setTimer(sec) {
     if (sec === 0) {
-        	$('#startBtn').hide()
-        $('#seconds').css({'font-size':'24px','color':'black'})
+        $('#startBtn').hide()
+        $('#seconds').css({
+            'font-size': '24px',
+            'color': 'black'
+        })
         return;
     };
     sec--;
-    	$('#startBtn').hide()
-    if (sec ===10){
-    	$('#seconds').css({'font-size':'72px','color':'red'})
+    $('#startBtn').hide()
+    if (sec === 10) {
+        $('#seconds').css({
+            'font-size': '72px',
+            'color': 'red'
+        })
     }
     $('#seconds').text(sec);
     window.setTimeout(setTimer, 1000, sec);
